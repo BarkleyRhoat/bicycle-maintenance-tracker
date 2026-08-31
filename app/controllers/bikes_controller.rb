2d4faf8bd7fa1071b93438bbcf135addae9fb7d5
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-08-31 15:44:50 -0400

    Added BikeController, routes, and views

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,49 @@
+class BikesController < ApplicationController
+  before_action :require_login
+  before_action :set_bike, only: %i[show edit update destroy]
+
+  def index
+    @bikes = current_user.bikes
+  end
+
+  def show; end
+
+  def new
+    @bike = current_user.bikes.build
+  end
+
+  def create
+    @bike = current_user.bikes.build(bike_params)
+
+    if @bike.save
+      redirect_to bike_path(@bike), notice: "Bike added successfully."
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @bike.update(bike_params)
+      redirect_to bike_path(@bike), notice: "Bike updated successfully."
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @bike.destroy
+    redirect_to bikes_path, notice: "Bike deleted successfully."
+  end
+
+  private
+
+  def set_bike
+    @bike = current_user.bikes.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(:name, :brand)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "bikes#index"
+
+  resources :bikes
+
   get "signup", to: "users#new", as: :signup
   post "signup", to: "users#create"
 
@@ -6,10 +10,5 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy", as: :logout
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  root "sessions#new"
 end
